40f3643c8428df2192894120e5254eda9555fbbd
viveav <morozovavioletta1808@gmail.com>
2026-09-16 17:55:29 +0500

    Выполнила Задание 2

--- src/task2-any.ts
+++ src/task2-any.ts
@@ -1,14 +1,14 @@
-export function calculateDiscount(price: any, discount: any): any {
+export function calculateDiscount(price: number, discount: number): number {
   return price - discount;
 }
 
-export function formatGreeting(name: any, isFormal: any): any {
+export function formatGreeting(name: string, isFormal: boolean): string {
   if (isFormal) {
     return "Добрый день, " + name;
   }
   return "Привет, " + name;
 }
 
-export function repeatText(text: any, count: any): any {
+export function repeatText(text: string, count: number): string {
   return text.repeat(count);
 }
